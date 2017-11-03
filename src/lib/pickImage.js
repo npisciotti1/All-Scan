eee0b7cdd98ec0106e1e7b1bfa0dc32100d32bc8
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-11-03 16:14:32 -0700

    refactor pickImage to be separate module

---
+++ src/lib/pickImage.js
@@ -0,0 +1,25 @@
+import ImagePicker from 'react-native-image-picker';
+
+export default pickImageProm = () => {
+  return new Promise((resolve, reject) => {
+    ImagePicker.showImagePicker( response => {
+      if (response.didCancel) {
+        return reject(console.log('User cancelled image picker'));
+      }
+      else if (response.error) {
+        return reject(console.log('ImagePicker Error: ', response.error));
+      }
+      else if (response.customButton) {
+        return reject(console.log('User tapped custom button: ', response.customButton));
+      }
+      else {
+        let result = {
+          source: { uri: response.uri },
+          data: response.data,
+        };
+        console.log('was i picked?')
+        return resolve(result);
+      }
+    });
+  })
+}
