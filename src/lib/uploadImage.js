722913040dc9e45a4379ab5b8e4f894a8ab79bfe
npisciotti1 <nikkopisciotti@gmail.com>
2017-11-16 15:13:05 -0800

    change endpoint name to new address

--- src/lib/uploadImage.js
+++ src/lib/uploadImage.js
@@ -2,12 +2,10 @@ import RNFetchBlob from 'react-native-fetch-blob';
 
 
 export default uploadImage = (base64Img) => {
-  /* https://async-redux-backend.herokuapp.com/ */
-
   return new Promise((resolve, reject) => {
     RNFetchBlob.fetch(
       'POST',
-      'https://async-redux-backend.herokuapp.com/api/analyze',
+      'https://textract-server.herokuapp.com/api/analyze',
       { 'Content-Type': 'multipart/form-data' },
       [{ name: 'imageToExtract', filename: 'imageToExtract.jpeg', data: base64Img }
     ])
