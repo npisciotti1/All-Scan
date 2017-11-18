6cbc94ef0b9fd45b1582711554fdde5d9c949872
npisciotti1 <nikkopisciotti@gmail.com>
2017-11-18 15:26:03 -0800

    refactor vision call

--- src/lib/uploadImage.js
+++ src/lib/uploadImage.js
@@ -2,13 +2,27 @@ import RNFetchBlob from 'react-native-fetch-blob';
 
 
 export default uploadImage = (base64Img) => {
+
+  let body = {
+    requests: [
+      {
+        image: {
+          content: base64Img
+        },
+        features: [
+          {
+            type: "TEXT_DETECTION"
+          }
+        ]
+      }
+    ]
+  };
   return new Promise((resolve, reject) => {
     RNFetchBlob.fetch(
       'POST',
-      'https://textract-server.herokuapp.com/api/analyze',
-      { 'Content-Type': 'multipart/form-data' },
-      [{ name: 'imageToExtract', filename: 'imageToExtract.jpeg', data: base64Img }
-    ])
+      `https://vision.googleapis.com/v1/images:annotate?key=${process.env.GOOGLE_API_KEY}`,
+      { 'Content-Type': 'application/json' }, JSON.stringify(body)
+      )
     .then( res => resolve(res))
     .catch( err => reject(res))
   })
